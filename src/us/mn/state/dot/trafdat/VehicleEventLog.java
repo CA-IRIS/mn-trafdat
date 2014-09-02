e26af398338b9250f03e9dccbaa327fd63ca26ca
Douglas Lau (doug.lau@state.mn.us) <Douglas Lau (doug.lau@state.mn.us)>
2014-09-02 12:35:43 -0500

    Minor whitespace cleanup.

--- src/us/mn/state/dot/trafdat/VehicleEventLog.java
+++ src/us/mn/state/dot/trafdat/VehicleEventLog.java
@@ -1,6 +1,6 @@
 /*
  * Project: Trafdat
- * Copyright (C) 2007-2011  Minnesota Department of Transportation
+ * Copyright (C) 2007-2014  Minnesota Department of Transportation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,18 +27,18 @@ import java.util.ListIterator;
 public class VehicleEventLog {
 
 	/** Get the 30-second period for the given timestamp (ms) */
-	static protected int getPeriod30Second(int ms) {
+	static private int getPeriod30Second(int ms) {
 		return ms / 30000;
 	}
 
 	/** List of all vehicle events in the log */
-	protected final LinkedList<VehicleEvent> events =
+	private final LinkedList<VehicleEvent> events =
 		new LinkedList<VehicleEvent>();
 
 	/** Create a new vehicle event log */
 	public VehicleEventLog(BufferedReader reader) throws IOException {
 		String line = reader.readLine();
-		while(line != null) {
+		while (line != null) {
 			events.add(new VehicleEvent(line));
 			line = reader.readLine();
 		}
@@ -47,8 +47,8 @@ public class VehicleEventLog {
 	/** Propogate timestamps forward to following events */
 	public void propogateStampsForward() {
 		Integer stamp = null;
-		for(VehicleEvent e: events) {
-			if(stamp != null)
+		for (VehicleEvent e: events) {
+			if (stamp != null)
 				e.setPreviousStamp(stamp);
 			stamp = e.getStamp();
 		}
@@ -59,7 +59,7 @@ public class VehicleEventLog {
 		Integer stamp = null;
 		ListIterator<VehicleEvent> it =
 			events.listIterator(events.size());
-		while(it.hasPrevious()) {
+		while (it.hasPrevious()) {
 			VehicleEvent e = it.previous();
 			e.setStamp(stamp);
 			stamp = e.getPreviousStamp();
@@ -70,16 +70,16 @@ public class VehicleEventLog {
 	public void interpolateMissingStamps() {
 		Integer stamp = null;
 		LinkedList<VehicleEvent> ev = new LinkedList<VehicleEvent>();
-		for(VehicleEvent e: events) {
+		for (VehicleEvent e: events) {
 			Integer s = e.getStamp();
-			if(s == null)
+			if (s == null)
 				ev.add(e);
-			else if(!ev.isEmpty()) {
-				if(stamp != null) {
+			else if (!ev.isEmpty()) {
+				if (stamp != null) {
 					int gap = s - stamp;
 					int t = ev.size() + 1;
 					int headway = Math.round(gap / t);
-					for(VehicleEvent v: ev) {
+					for (VehicleEvent v: ev) {
 						v.setHeadway(headway);
 						v.setPreviousStamp(stamp);
 						stamp = v.getStamp();
@@ -87,7 +87,7 @@ public class VehicleEventLog {
 				}
 				ev.clear();
 			}
-			if(s != null)
+			if (s != null)
 				stamp = s;
 		}
 	}
@@ -95,17 +95,17 @@ public class VehicleEventLog {
 	/** Bin vehicle event data into 30 second samples */
 	public void bin30SecondSamples(SampleBin bin) {
 		SampleData sam = new SampleData();
-		for(VehicleEvent e: events) {
+		for (VehicleEvent e: events) {
 			Integer stamp = e.getStamp();
-			if(e.isReset() || stamp == null)
+			if (e.isReset() || stamp == null)
 				sam.setReset();
 			else {
 				int p = getPeriod30Second(stamp);
 				int sp = sam.getPeriod();
-				if(sam.isReset())
+				if (sam.isReset())
 					sam.clear(p + 1);
-				else if(p >= sp) {
-					while(p > sp) {
+				else if (p >= sp) {
+					while (p > sp) {
 						bin.addSample(sam);
 						sp++;
 						sam.clear(sp);
